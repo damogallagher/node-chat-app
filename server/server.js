241628fbf13702dcf561c1b5568f0af1e8f575e8
Damien Gallagher <damien.gallagher@gmail.com>
2018-09-21 15:16:39 +0100

    Added geolocation for the user

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const { generateMessage } = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 
 const publicPath = path.join(__dirname, '../public');
@@ -23,7 +23,10 @@ io.on('connection', (socket) => {
         console.log('createMessage:', newMessage);
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
         callback('This is from the server');
-        // socket.broadcast.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -11,5 +11,20 @@ describe('generateMessage', () => {
         expect(message.from).toBe(from);
         expect(message.text).toBe(text);
         expect(message.createdAt).toBeTruthy();
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate the correct location message object', () => {
+        var from = 'David';
+        var latitude = 15;
+        var longitude = 19;
+        var url = 'https://www.google.com/maps?q=15,19'
+
+        var locationMessage = generateLocationMessage(from, latitude, longitude);
+        expect(locationMessage.from).toBe(from);
+        expect(locationMessage.url).toBeTruthy();
+        expect(locationMessage.url).toBe(url);
+        expect(locationMessage.createdAt).toBeTruthy();
     });
 });
