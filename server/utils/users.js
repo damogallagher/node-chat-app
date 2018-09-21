592a109dec9f066c9aa032895a9c4cc79a2a7f59
Damien Gallagher <damien.gallagher@gmail.com>
2018-09-21 19:29:51 +0100

    Added eslint support

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,35 +1,33 @@
 class Users {
+  constructor() {
+    this.users = [];
+  }
 
-    constructor() {
-        this.users = [];
+  addUser(id, name, room) {
+    const user = { id, name, room };
+    this.users.push(user);
+    return user;
+  }
+
+  removeUser(id) {
+    const user = this.getUser(id);
+    if (user) {
+      this.users = this.users.filter(filteredUser => filteredUser.id !== id);
     }
 
-    addUser (id, name, room) {
-        var user = {id, name, room};
-        this.users.push(user);
-        return user;
-    }
+    return user;
+  }
 
-    removeUser (id) {
-        var user = this.getUser(id);
-        if (user) {
-            this.users = this.users.filter((user) => user.id !== id);
-        }
+  getUser(id) {
+    return this.users.filter(user => user.id === id)[0];
+  }
 
-        return user;
-    }
+  getUserList(room) {
+    const users = this.users.filter(user => user.room === room);
+    const namesArray = users.map(user => user.name);
 
-    getUser (id) {
-        return this.users.filter((user) => user.id === id)[0];
-    }
+    return namesArray;
+  }
+}
 
-    getUserList (room) {
-        var users = this.users.filter((user) => user.room === room);
-        var namesArray = users.map((user) => user.name);
-
-        return namesArray;
-    }
-
-};
-
-module.exports = {Users};
+module.exports = { Users };
