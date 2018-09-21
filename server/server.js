69f0a66fffc3076bc84cbebdb48e97dd3bec26c2
Damien Gallagher <damien.gallagher@gmail.com>
2018-09-21 14:13:04 +0100

    Added code coverage utilities

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const path                = require('path');
-const http                = require('http');
-const express             = require('express');
-const socketIO            = require('socket.io');
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 const { generateMessage } = require('./utils/message');
 
 
@@ -16,19 +16,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
-    socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (newMessage) => {
+    socket.on('createMessage', (newMessage, callback) => {
         console.log('createMessage:', newMessage);
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
-
+        callback('This is from the server');
         // socket.broadcast.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
     });
 
     socket.on('disconnect', () => {
         console.log('Client disconnected from server');
-    });    
+    });
 });
 
 
