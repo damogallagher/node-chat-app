ca26d826d031b367c3202c66829cc5d288ffc78e
Damien Gallagher <damien.gallagher@gmail.com>
2018-09-21 15:44:13 +0100

    Added styles for the chatroom

--- public/js/index.js
+++ public/js/index.js
@@ -37,11 +37,12 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextBox = jQuery("[name=message]");
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery("[name=message]").val()
+        text: messageTextBox.val()
     }, function() {
-
+        messageTextBox.val('');
     });
 });
 
@@ -50,13 +51,16 @@ locationButton.on('click', function(e) {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
-
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+    
     navigator.geolocation.getCurrentPosition(function(position) {
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
+        locationButton.removeAttr('disabled').text('Send location');
     }, function (e) {
         alert('Unable to fetch location');
+        locationButton.removeAttr('disabled').text('Send location');
     });
 });
